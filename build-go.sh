3809719b801903f93f3944cb9ca42343e291774d
ashiqul mostofa <ashiqulmostofa@ashiquls-MacBook-Pro.local>
2018-03-12 03:31:35 +0800

    rest

---
+++ build-go.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+# Clone an example Go repo
+# This step obviously won't be necessary if you're building a Go repo with LambCI,
+# because you'll already be in the cloned repo directory
+export LAMBCI_REPO=/acophonix/be-better
+rm -rf /tmp/${LAMBCI_REPO}
+git clone --depth 1 https://github.com/${LAMBCI_REPO} /tmp/${LAMBCI_REPO}
+cd /tmp/${LAMBCI_REPO}
+
+
+# You could just put this in your `cmd` config property
+. ~/init/go && go get -t -v ./... && go test -v ./...
